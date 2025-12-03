63cf26212bf1f292d3bd2c97f577216ebf72ad88
Vito <yutcpp41100202@gmail.com>
2025-12-03 15:20:16 +0800

    init: reset project(clean history)

---
+++ Fatty/BotHostingService.cs
@@ -0,0 +1,24 @@
+namespace Fatty
+{
+    public class BotHostingService : BackgroundService
+    {
+        private readonly ILogger<BotHostingService> _logger;
+
+        public BotHostingService(ILogger<BotHostingService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ Fatty/Program.cs
@@ -0,0 +1,31 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Lavalink4NET.Extensions;
+using Fatty;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<DiscordSocketClient>(sp =>
+{
+    var config = new DiscordSocketConfig
+    {
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMessages | GatewayIntents.GuildVoiceStates,
+        LogLevel = LogSeverity.Info,
+        AlwaysDownloadUsers = false
+    };
+    return new DiscordSocketClient(config);
+});
+
+builder.Services.AddSingleton<InteractionService>(sp =>
+{
+    var client = sp.GetRequiredService<DiscordSocketClient>();
+    return new InteractionService(client);
+});
+
+builder.Services.AddLavalink();
+
+builder.Services.AddHostedService<BotHostingService>();
+
+var host = builder.Build();
+await host.RunAsync();
